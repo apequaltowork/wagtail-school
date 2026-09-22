b5b92b162b74580d166bb1d4bb0a6d5793d2ad44
Ashish Pitroda <apequaltowork@gmail.com>
2026-09-22 10:15:53 +0530

    build(forms): add enrolment FormPage and ContactPage with FormSubmissionsPanel

---
+++ forms/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
+)
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.core.fields import RichTextField
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(_('intro'), blank=True)
+    thank_you_text = RichTextField(_('thank you text'), blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname='full'),
+        InlinePanel('form_fields', label=_('Form fields')),
+        FieldPanel('thank_you_text', classname='full'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname='col6'),
+                FieldPanel('to_address', classname='col6'),
+            ]),
+            FieldPanel('subject'),
+        ], heading=_('Email')),
+    ]
+
+    class Meta:
+        verbose_name = _('form page')
+
+
+class ContactFormField(AbstractFormField):
+    page = ParentalKey('ContactPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class ContactPage(AbstractEmailForm):
+    intro = RichTextField(_('intro'), blank=True)
+    thank_you_text = RichTextField(_('thank you text'), blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname='full'),
+        InlinePanel('form_fields', label=_('Form fields')),
+        FieldPanel('thank_you_text', classname='full'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname='col6'),
+                FieldPanel('to_address', classname='col6'),
+            ]),
+            FieldPanel('subject'),
+        ], heading=_('Email')),
+    ]
+
+    subpage_types = []
+
+    class Meta:
+        verbose_name = _('contact page')
